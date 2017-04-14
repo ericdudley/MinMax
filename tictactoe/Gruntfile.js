9f4b34b0269ddbc7d7ced9667bd3d021a18079f4
Eric Dudley <ericdudley@email.com>
2017-04-14 14:30:57 -0400

    Added grunt watch.

--- tictactoe/Gruntfile.js
+++ tictactoe/Gruntfile.js
@@ -12,14 +12,19 @@ module.exports = function(grunt) {
       bundle: {
         files: { '<%= pkg.name %>.min.js':'src/bundled.js' },
       }
+    },
+    watch: {
+        files: ['src/*.js'],
+        tasks: ['concat:js']
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat:js']);
+  grunt.registerTask('default', ['concat:js', 'watch']);
 
 };
